6141dc7fe398e0fe766c8fb4bed0ea4c28d8c88d
saikumarputta <saianuram1@gmail.com>
2020-01-07 14:08:17 +0530

    hit_redis-server

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using StackExchange.Redis;
+using System;
+
+namespace RedisConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConnectionMultiplexer redis = ConnectionMultiplexer.Connect("localhost");
+            IDatabase db = redis.GetDatabase();
+            var val = db.StringGet("k1");
+            Console.WriteLine(val);
+            Console.ReadLine();
+        }
+    }
+}
